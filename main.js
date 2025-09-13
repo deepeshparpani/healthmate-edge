2a7611550255fcc053411521a0ceb6186a2308ed
Aviral Gupta <129996527+aviral23032002@users.noreply.github.com>
2025-09-13 17:10:35 -0400

    FIxed path issues and file name changes

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const isDev = process.env.NODE_ENV === 'development';
+
+// __dirname workaround for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,7 +22,7 @@ function createWindow() {
     win.loadURL('http://localhost:5173'); // Vite dev server
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(__dirname, 'dist/index.html'));
+    win.loadFile(path.join(__dirname, 'public/index.html'));
   }
 }
 
